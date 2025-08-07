81ea11ad2041a38d6ff6804cbdbb46a69e5bfa3d
Atta Haider <attahaider05@gmail.com>
2025-08-07 14:18:18 +0500

    second commit

--- src/Services/services.jsx
+++ src/Services/services.jsx
@@ -2,6 +2,6 @@ import axios from "axios"
 const API = axios.create({
     baseURL: "http://www.omdbapi.com"
 })
-export const getMovies = () =>{
-    return API.get("/?s=Batman&type=movie&apikey=36235230&page=1")
+export const getMovies = (page) =>{
+    return API.get(`/?s=Batman&type=movie&apikey=36235230&page=${page}`)
 }
--- src/components/UI/Movies.jsx
+++ src/components/UI/Movies.jsx
@@ -2,13 +2,17 @@ import { useEffect, useState } from "react"
 import { Details } from "./Details";
 import "./Movies.css"
 import { getMovies } from "../../Services/services";
+import { FcNext } from "react-icons/fc";
+import { FcPrevious } from "react-icons/fc";
+
 // 
 export const Movies = () =>{
     const [data, setData] = useState([]);
-    const API = "http://www.omdbapi.com/?s=Batman&type=movie&apikey=36235230&page=1"
+    const [page, setPage] = useState(1);
+    const API = getMovies();
     const fetchData = async () =>{
         try {
-            const res = await getMovies();
+            const res = await getMovies(page);
             console.log(res.data.Search)
             setData(res.data.Search)
             
@@ -20,9 +24,27 @@ export const Movies = () =>{
     useEffect(()=>{
         fetchData();
     }, [])
+    
+    //Function to handle page clicks
+    const handleNextClick = () =>{
+        console.log("i am clicked")
+        setPage(page+1)
+        fetchData();
+    }
+    const handlePreviousClick = () =>{
+        if(page-1 != 0){
+            setPage(page-1)
+            fetchData();
+        }
+    }
     return (<>
     <div className="heroContainer">
         <h1>Best Batman Movies</h1>
+        <div className="nextPreviousContainer">
+            <FcPrevious onClick={()=> handlePreviousClick()} className="clickPrevious"/>
+            <p>{page}</p>
+            <FcNext onClick={()=> handleNextClick()} className="clickNext"/>
+        </div>
     <ul>
         {data.map((currentElem)=>{
             return <Details key={currentElem.imdbID} data={currentElem}/>
